abce84b98f918f2f9d54fb6165b66db83ef657ad
Max <max2002online@gmail.com>
2020-09-05 12:30:47 +0200

    Streaming Works

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,16 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
+
 function StreamShow({ fetchStream, match, stream }) {
+    const videoRef = useRef(null);
+    let player = null;
     useEffect(() => {
         fetchStream(match.params.id);
+        buildPlayer();
     }, [fetchStream, match.params.id]);
+
+    useEffect(() => {
+        buildPlayer();
+    });
+
+    const buildPlayer = () => {
+        if (player || !stream) {
+            return;
+        }
+        player = flv.createPlayer({
+            type: "flv",
+            url: `http://localhost:8000/live/${match.params.id}.flv`,
+        });
+        player.attachMediaElement(videoRef.current);
+        player.load();
+    };
+
     if (!stream) {
         return <div>Loading...</div>;
     }
 
     return (
         <div>
+            <video controls style={{ width: "100%" }} ref={videoRef} />
             <h1>{stream.title}</h1>
             <h5>{stream.description}</h5>
         </div>
